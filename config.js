c59b96fdfcc031123281e8c0560964a8c1036c2b
Mudabbir-Hassan <mudabbirdar5@gmail.com>
2023-11-16 11:46:50 +0500

    MVC set up created || initial commit created

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports= {
+    db: {
+        "database": "Ecommerce",
+        "username": "postgres",
+        "password": "hassan12",
+        "host": "localhost",
+        "port": "5432",
+        "dialect": "postgres"
+        }
+    };
